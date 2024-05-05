bcaabff7e0acf476e71f8cc89d822d3658cf5f00
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-05-05 13:09:50 +0000

    auth functionalities

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+
+urlpatterns = [
+    path("get-token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("refresh-token/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("social-login/", include("drf_social_oauth2.urls", namespace="social_login")),
+]

e82a95a532c9db9fac597623abc7d727de49d185
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-05-16 08:34:39 +0000

    Duplicate flights

--- flight/management/commands/generate_flights.py
+++ flight/management/commands/generate_flights.py
@@ -23,25 +23,24 @@ class Command(BaseCommand):
         while start_date < end_date:
             num_flights = random.randint(3, 5)
             for _ in range(num_flights):
-                self.generate_random_flight(start_date)
+                self.generate_random_flight(start_date, duplicates=3)
             start_date += datetime.timedelta(days=1)
 
         self.stdout.write(self.style.SUCCESS(f'Flights generated successfully'))
 
-    def generate_random_flight(self, date):
-        airlines = list(Airline.objects.all())
-        departure_location = Location.objects.order_by('?').first()
-        arrival_location = Location.objects.exclude(id=departure_location.id).order_by('?').first()
-        departure_datetime = datetime.datetime.combine(date, fake.time_object())
-        flight_duration = datetime.timedelta(hours=random.randint(1, 24), minutes=random.randint(0, 59))
-        base_price = round(random.uniform(50, 1000), 2)
-        passenger_type = random.choice(['Adult', 'Minor'])
-        flight_class = random.choice(['Business', 'Economic'])
-        checked_bag_price = round(random.uniform(0, 50), 2)
-        num_tickets = random.randint(50, 300)
+    def generate_random_flight(self, date, duplicates=3):
+        for _ in range(duplicates):
+            airline = Airline.objects.order_by('?').first()
+            departure_location = Location.objects.order_by('?').first()
+            arrival_location = Location.objects.exclude(id=departure_location.id).order_by('?').first()
+            departure_datetime = datetime.datetime.combine(date, fake.time_object())
+            flight_duration = datetime.timedelta(hours=random.randint(1, 24), minutes=random.randint(0, 59))
+            base_price = round(random.uniform(50, 1000), 2)
+            passenger_type = random.choice(['Adult', 'Minor'])
+            flight_class = random.choice(['Business', 'Economic'])
+            checked_bag_price = round(random.uniform(0, 50), 2)
+            num_tickets = random.randint(50, 300)
 
-        for _ in range(3):  # Duplicate the flight with different airlines
-            airline = random.choice(airlines)
             flight = Flight.objects.create(
                 airline=airline,
                 departure_location=departure_location,
